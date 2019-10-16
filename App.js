565e4076fd08bc2a75870193ff05e4ca0ddeec51
Manoj Dobbala <mdobs.ui@gmail.com>
2019-10-16 11:42:38 -0500

    Reference update

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from '/pages/HomePage';
+import HomePage from '/pages/Homepage';
 import LoginPage from '/pages/Loginpage';
 import RegisterPage from '/pages/Registerpage';
 import Errorpage from "/pages/Errorpage";
