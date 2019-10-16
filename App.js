ca6b02b8b854c267581eb2daf84af8bd49605c0d
Manoj Dobbala <mdobs.ui@gmail.com>
2019-10-16 11:31:54 -0500

    Registration added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/Loginpage';
+import RegisterPage from './pages/Registerpage';
 import Errorpage from "./pages/Errorpage";
 
 export class App extends Component {
@@ -11,6 +12,7 @@ export class App extends Component {
                 <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route path="/login" component={LoginPage}/>
+                    <Route path="/signup" component={RegisterPage}/>
                     <Route path="/error" component={Errorpage}/>
                 </Switch>
         );
--- pages/Loginpage.js
+++ pages/Loginpage.js
@@ -12,6 +12,7 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import {NavLink} from "react-router-dom";
 
 function Copyright() {
     return (
@@ -113,9 +114,10 @@ export default function SignInSide() {
                                 </Link>
                             </Grid>
                             <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
+                                <NavLink to="/signup"> Don't have an account? Sign Up</NavLink>
+                                {/*<Link href="/signup" variant="body2">*/}
+                                {/*    {""}*/}
+                                {/*</Link>*/}
                             </Grid>
                         </Grid>
                         <Box mt={5}>
